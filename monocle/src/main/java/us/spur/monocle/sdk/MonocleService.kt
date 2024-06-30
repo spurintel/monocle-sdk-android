68dd61e11a628dee4bd78ab055b0817799582f64
Jack <jack@spur.us>
2024-06-30 15:27:00 -0400

    Documentation update

--- monocle/src/main/java/us/spur/monocle/sdk/MonocleService.kt
+++ monocle/src/main/java/us/spur/monocle/sdk/MonocleService.kt
@@ -7,10 +7,10 @@ import retrofit2.http.Query
 
 /**
  * Monocle API Service definition for Android
- * @param tk Monocle site-token from https://app.spur.us/monocle
- * @param v Monocle version
- * @param s Monocle script ID
- * @param t Monocle source
+ * @param tk Monocle site-token String unique to your deployment. From https://app.spur.us/monocle
+ * @param v Monocle version String
+ * @param s Monocle script ID String.  This is expected to be a UUID unique to the device or app install (like ANDROID_ID), but must be unique for at least the session.
+ * @param t Monocle source String.  This identifies the platform using Monocle for backend analytics.
  */
 interface MonocleService {
     @POST("/r/bundle")
